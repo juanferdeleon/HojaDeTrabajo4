f47312a87e8fd9c003d3f08ca8a77274c1d9ad44
Juan De Leon <dele17822@uvg.edu.gt>
2019-02-15 09:49:34 -0600

    Interfaz iPila

---
+++ src/AbstractStack.java
@@ -0,0 +1,7 @@
+/**
+ * Clase abstracta de la Pila
+ * @author Juanfer De Leon
+ */
+public class AbstractStack<E> implements iPila<E>{
+
+}
---
+++ src/iPila.java
@@ -0,0 +1,37 @@
+/**
+ * Interface iPila
+ * @param <E> Dato generico
+ * @author Juanfer De Leon
+ */
+public interface iPila<E> {
+    /**
+     * An item is added to stack
+     * @param item
+     */
+    void push(E item);
+
+    /**
+     * The most recently pushed item is removed and returned
+     * @return
+     */
+    E pop();
+
+
+    /**
+     * The top value (next to be popped) is returned
+     * @return
+     */
+    E peek();
+
+    /**
+     * returns true if and only if the stack is empty
+     * @return
+     */
+    boolean empty();
+
+    /**
+     * returns the number of elements in the stack
+     * @return
+     */
+    int size();
+}
