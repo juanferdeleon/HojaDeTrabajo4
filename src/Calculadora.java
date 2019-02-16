2d793837fdf4f6623a2023b8440d12f6f5a415f5
Juan De Leon <dele17822@uvg.edu.gt>
2019-02-16 17:13:04 -0600

    Singleton and operar postfix

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,14 +1,14 @@
 /**
  * La clase calculadora realiza los calculos de una expresion en formato postfix
  */
-public class Calculadora implements iCalculadora {
+public class Calculadora {
 
-    double resultado;
+    static double resultado;
 
-    private static iCalculadora firstInstance = null;
+    private static Calculadora firstInstance = null;
 
     /**
-     * Calculator constructur
+     * Calculator constructor
      * Is private due to we want to have only one
      * instance of it
      */
@@ -19,7 +19,7 @@ public class Calculadora implements iCalculadora {
      * an instance of the object Calculadora
      * @return the instance
      */
-    public iCalculadora getInstance(){
+    public static Calculadora getInstance(){
 
         if (firstInstance == null){
             firstInstance = new Calculadora();
@@ -35,9 +35,9 @@ public class Calculadora implements iCalculadora {
      * @param b double - sumando
      * @return - El resultado de la suma de a y b
      */
-    public double sumar(double a, double b) {
-        this.resultado = a + b;
-        return this.resultado;
+    public static double sumar(double a, double b) {
+        resultado = a + b;
+        return resultado;
     }
 
     /**
@@ -47,9 +47,9 @@ public class Calculadora implements iCalculadora {
      * @param b double -
      * @return double - El resultado de la resta entre a y b
      */
-    public double restar(double a, double b) {
-        this.resultado = a - b;
-        return this.resultado;
+    public static double restar(double a, double b) {
+        resultado = a - b;
+        return resultado;
     }
 
     /**
@@ -59,9 +59,9 @@ public class Calculadora implements iCalculadora {
      * @param b double - factor
      * @return double - El resultado de la multiplicación entre a y b
      */
-    public double multiplicar(double a, double b) {
-        this.resultado = a * b;
-        return this.resultado;
+    public static double multiplicar(double a, double b) {
+        resultado = a * b;
+        return resultado;
     }
 
     /**
@@ -71,8 +71,70 @@ public class Calculadora implements iCalculadora {
      * @param b double - divisor
      * @return double - El resultado de la división entre a y b
      */
-    public double dividir(double a, double b) {
-        this.resultado = a / b;
-        return this.resultado;
+    public static double dividir(double a, double b) {
+        resultado = a / b;
+        return resultado;
     }
+
+    /**
+     * Method that evaluates postfix text
+     * @param postfixText postfix text
+     * @param stack selected stack
+     * @return stack with final answer
+     */
+    public static iPila<Double> operar(String postfixText, iPila<Double> stack){
+
+        for (int position = 0; position < postfixText.length(); ++position){
+
+            char caracter = postfixText.charAt(position);
+
+            if (Character.isLetter(caracter)){
+
+                System.out.println("ERROR: El archivo contiene un caracter");
+                break;
+
+            }
+
+            if (Character.isDigit(caracter)){
+
+                stack.push((double)Character.digit(caracter, 10));
+
+            } else if (String.valueOf(caracter).matches("[+]")){
+
+                double a = stack.pop();
+                double b = stack.pop();
+                double sum = sumar(a, b);
+                stack.push(sum);
+
+            } else if (String.valueOf(caracter).matches("[-]")){
+
+                double a = stack.pop();
+                double b = stack.pop();
+                double sum = restar(a, b);
+                stack.push(sum);
+
+            } else if (String.valueOf(caracter).matches("[*]")){
+
+                double a = stack.pop();
+                double b = stack.pop();
+                double sum = multiplicar(a, b);
+                stack.push(sum);
+
+            } else if (String.valueOf(caracter).matches("[/]") && stack.peek() != 0.0D){
+
+                double a = stack.pop();
+                double b = stack.pop();
+                double sum = dividir(b, a);
+                stack.push(sum);
+
+            } else if (String.valueOf(caracter).matches("[/]") && stack.peek() == 0.0D){
+
+                System.out.println("ERROR: Se intenta realizar una division dentro de cero.");
+                break;
+            }
+        }
+
+        return stack;
+    }
+
 }
