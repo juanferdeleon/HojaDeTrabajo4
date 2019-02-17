bf010804017dbdd71517542a208df699ea358514
Juan De Leon <dele17822@uvg.edu.gt>
2019-02-16 22:26:23 -0600

    Correccion

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,43 +1,87 @@
-public class DoublyLinkedList<E> extends AbstractList<E>{
-	protected int count;
-	protected DoublyLinkedNode<E> head;
-	protected DoublyLinkedNode<E> tail;
-	protected Node<E> head2;
-	public DoublyLinkedList()
-	// post: constructs an empty list
-	{
-	head = null;
-	tail = null;
-	count = 0;
-	}
-	public void addFirst(E value)
-	// pre: value is not null
-	// post: adds element to head of list
-	{
-	// construct a new element, making it head
-	head = new DoublyLinkedNode<E>(value, head, null);
-	// fix tail, if necessary
-	if (tail == null) tail = head;
-	count++;
-	}
-	public void addLast(E value)
-	// pre: value is not null
-	// post: adds new value to tail of list
-	{
-	// construct new element
-	tail = new DoublyLinkedNode<E>(value, null, tail);
-	// fix up head
-	if (head == null) head = tail;
-	count++;
-	}
-	public E removeFirst()
-	// pre: list is not empty
-	// post: removes and returns value from beginning of list
-	{
-	Node<E> temp = head2;
-	head2 = head2.next(); // move head down list
-	count--;
-	return temp.value();
-	}
+import java.util.NoSuchElementException;
 
+/**
+ * DoublyLinkedList
+ * @param <E>
+ * @author Juanfer De Leon
+ */
+public class DoublyLinkedList<E> extends AbstractList<E>{
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    DoublyLinkedList() {
+        size = 0;
+    }
+    /**
+     * this class keeps track of each element information
+     * @author java2novice
+     *
+     */
+    private class Node {
+        E element;
+        Node next;
+        Node prev;
+
+        public Node(E element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    /**
+     * Gets last element on the list
+     * @return
+     */
+    public E getLast(){
+        return tail.element;
+    }
+
+
+    /**
+     * returns the size of the linked list
+     * @return
+     */
+    public int size() { return size; }
+
+    /**
+     * return whether the list is empty or not
+     * @return
+     */
+    public boolean isEmpty() { return size == 0; }
+
+    /**
+     * adds element at the end of the linked list
+     * @param element
+     */
+    public void addLast(E element) {
+
+        Node tmp = new Node(element, null, tail);
+        if(tail != null) {tail.next = tmp;}
+        tail = tmp;
+        if(head == null) { head = tmp;}
+        size++;
+        System.out.println("adding: "+element);
+    }
+
+    /**
+     * this method removes element from the end of the linked list
+     * @return
+     */
+    public E removeLast() {
+        if (size == 0) throw new NoSuchElementException();
+        Node tmp = tail;
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        System.out.println("deleted: "+tmp.element);
+        return tmp.element;
+    }
 }
+
+
+
+
+
